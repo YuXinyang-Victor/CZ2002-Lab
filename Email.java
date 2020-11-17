1eb0986623fa17346d73dd164f067597d7efbcdb
summoribundus <61357039+summoribundus@users.noreply.github.com>
2020-11-17 16:11:15 +0800

    Add files via upload

---
+++ Email.java
@@ -0,0 +1,51 @@
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+class Email implements Notifier {
+	final String username, password;
+	public Email(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public void notify(String subject, String content, String receipients) {
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(sender));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(receipients)); // "to_username_a@gmail.com, to_username_b@yahoo.com"
+			message.setSubject(subject);
+			message.setText(content);
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+}
