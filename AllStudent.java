1da878903b0692bedfbdaf9a5ecc2abbf499cbd9
Pratyush Kumar Pandey <55307565+Pratyush0411@users.noreply.github.com>
2020-11-18 15:10:12 +0800

    Update AllStudent.java

--- AllStudent.java
+++ AllStudent.java
@@ -1,11 +1,54 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AllStudent implements FileHandle{
-    private static Map<String, Course> studentMap;
+    private static Map<String, Student> studentMap;
     public AllStudent(){}
 
     public static List readSerializedObject(String filename){
         return FileHandle.readSerializedObject(filename);
+    }
+    public static void serializeToFile() {
+        try {
+            if (studentMap != null) {
+                FileOutputStream fileOut =
+                    new FileOutputStream("Student.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(studentMap);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in courseInfo.ser");
+            }
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+    public static void deserializeFromFile() {
+        try {
+            FileInputStream fileIn = new FileInputStream("courseInfo.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            studentMap =(Map<String, Student>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException e) {
+             studentMap = new HashMap<String, Student>();
+        } catch (ClassNotFoundException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public static Map<String, Student> getStudentMap() {
+        return studentMap;
+    }
+
+    public static void setMap(Map<String, Student> studentMap) {
+        AllStudent.studentMap = studentMap;
     }
 }
